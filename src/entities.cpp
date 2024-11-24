d09ad585a64e614b1f80dc38813e787936a18cad
swiderskis <sebastianswiderski42@gmail.com>
2024-11-24 22:43:26 +0000

    Convert entity & component managers from class to struct

--- src/entities.cpp
+++ src/entities.cpp
@@ -7,9 +7,9 @@
 unsigned EntityManager::spawn_entity(const Entity type)
 {
     if (type == Entity::Player) {
-        assert(m_entities[PLAYER_ID] == std::nullopt);
+        assert(entities[PLAYER_ID] == std::nullopt);
 
-        m_entities[PLAYER_ID] = Entity::Player;
+        entities[PLAYER_ID] = Entity::Player;
 
         return PLAYER_ID;
     }
@@ -17,14 +17,14 @@ unsigned EntityManager::spawn_entity(const Entity type)
     assert(type != Entity::Player);
 
     unsigned entity_id = 0;
-    for (const auto [id, entity] : std::span(m_entities).subspan(1) | std::views::enumerate) {
+    for (const auto [id, entity] : std::span(entities).subspan(1) | std::views::enumerate) {
         if (entity != std::nullopt) {
             continue;
         }
 
         entity = type;
         entity_id = id + 1; // enumerate will start at 0, subspan starts loop from m_entitites[1]
-        m_entity_ids[type].push_back(entity_id);
+        entity_ids[type].push_back(entity_id);
         break;
     }
 
@@ -35,5 +35,5 @@ unsigned EntityManager::spawn_entity(const Entity type)
 
 void EntityManager::queue_destroy_entity(const unsigned id)
 {
-    m_entities_to_destroy.push_back(id);
+    entities_to_destroy.push_back(id);
 }
