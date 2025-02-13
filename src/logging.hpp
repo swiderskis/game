312d8d297f052674b182e05e30555c3891da17c3
swiderskis <sebastianswiderski42@gmail.com>
2025-02-13 15:23:33 +0000

    Simplify logging macros to allow easier redefinition of log levels for specific parts

--- src/logging.hpp
+++ src/logging.hpp
@@ -8,36 +8,32 @@
 #define LOGLVL 3
 #define FILENAME_WIDTH 8
 
-#if LOGLVL > 0
 #define LOG_ERR(...)                                                                                                   \
-    std::clog << "[ERR] " << std::string(__FILE__).substr(std::string(__FILE__).size() - FILENAME_WIDTH)               \
-              << std::format(" {:>5}: ", __LINE__) << std::format(__VA_ARGS__) << "\n";
-#else
-#define LOG_ERR(...)
-#endif
+    if (LOGLVL > 0)                                                                                                    \
+    {                                                                                                                  \
+        std::clog << "[ERR] " << std::string(__FILE__).substr(std::string(__FILE__).size() - FILENAME_WIDTH)           \
+                  << std::format(" {:>5}: ", __LINE__) << std::format(__VA_ARGS__) << "\n";                            \
+    }
 
-#if LOGLVL > 1
 #define LOG_WRN(...)                                                                                                   \
-    std::clog << "[WRN] " << std::string(__FILE__).substr(std::string(__FILE__).size() - FILENAME_WIDTH)               \
-              << std::format(" {:>5}: ", __LINE__) << std::format(__VA_ARGS__) << "\n";
-#else
-#define LOG_WRN(...)
-#endif
+    if (LOGLVL > 1)                                                                                                    \
+    {                                                                                                                  \
+        std::clog << "[WRN] " << std::string(__FILE__).substr(std::string(__FILE__).size() - FILENAME_WIDTH)           \
+                  << std::format(" {:>5}: ", __LINE__) << std::format(__VA_ARGS__) << "\n";                            \
+    }
 
-#if LOGLVL > 2
 #define LOG_INF(...)                                                                                                   \
-    std::clog << "[INF] " << std::string(__FILE__).substr(std::string(__FILE__).size() - FILENAME_WIDTH)               \
-              << std::format(" {:>5}: ", __LINE__) << std::format(__VA_ARGS__) << "\n";
-#else
-#define LOG_INF(...)
-#endif
+    if (LOGLVL > 2)                                                                                                    \
+    {                                                                                                                  \
+        std::clog << "[INF] " << std::string(__FILE__).substr(std::string(__FILE__).size() - FILENAME_WIDTH)           \
+                  << std::format(" {:>5}: ", __LINE__) << std::format(__VA_ARGS__) << "\n";                            \
+    }
 
-#if LOGLVL > 3
 #define LOG_TRC(...)                                                                                                   \
-    std::clog << "[TRC] " << std::string(__FILE__).substr(std::string(__FILE__).size() - FILENAME_WIDTH)               \
-              << std::format(" {:>5}: ", __LINE__) << std::format(__VA_ARGS__) << "\n";
-#else
-#define LOG_TRC(...)
-#endif
+    if (LOGLVL > 3)                                                                                                    \
+    {                                                                                                                  \
+        std::clog << "[TRC] " << std::string(__FILE__).substr(std::string(__FILE__).size() - FILENAME_WIDTH)           \
+                  << std::format(" {:>5}: ", __LINE__) << std::format(__VA_ARGS__) << "\n";                            \
+    }
 
 #endif
